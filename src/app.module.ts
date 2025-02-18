94e3b246b41fb73a218495943ed979b37c4b208a
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-02-19 00:38:38 +0700

    feat : import module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './controllers/user/user.module';
+import { MysqlModule } from './common/modules/mysql.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [MysqlModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
