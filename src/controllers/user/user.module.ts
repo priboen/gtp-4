323c00a53228c8ca1fd698cbfddb79e6297f86d3
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-02-19 00:38:52 +0700

    feat : add user providers

--- src/controllers/user/user.module.ts
+++ src/controllers/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { userProvider } from 'src/common/providers';
 
 @Module({
+  imports: [],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, userProvider],
 })
 export class UserModule {}
