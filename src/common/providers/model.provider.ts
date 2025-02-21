292399dafd576130c3b540651f385f4478e32af9
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-02-22 00:52:46 +0700

    export model provider

--- src/common/providers/model.provider.ts
+++ src/common/providers/model.provider.ts
@@ -1,6 +1,21 @@
-import { User } from '../models';
+import { Events, Project, Task, User } from '../models';
 
 export const userProvider = {
   provide: 'USER_REPOSITORY',
   useValue: User,
+};
+
+export const projectProvider = {
+  provide: 'PROJECT_REPOSITORY',
+  useValue: Project,
+};
+
+export const taskProvider = {
+  provide: 'TASK_REPOSITORY',
+  useValue: Task,
+};
+
+export const eventsProvider = {
+  provide: 'EVENTS_REPOSITORY',
+  useValue: Events,
 };
