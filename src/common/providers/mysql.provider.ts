916a611296a4b9eb2cb4545f53a7a389c6fb0566
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-02-22 00:52:24 +0700

    implement model to db provider

--- src/common/providers/mysql.provider.ts
+++ src/common/providers/mysql.provider.ts
@@ -1,12 +1,12 @@
 import { Sequelize } from 'sequelize-typescript';
 import { mysqlConfig } from '../config/mysql.config';
-import { User } from '../models';
+import { User, Task, Events, Project } from '../models';
 
 export const mysqlProvider = {
   provide: 'SEQUELIZE',
   useFactory: async () => {
     const sequelize = new Sequelize(mysqlConfig);
-    sequelize.addModels([User]);
+    sequelize.addModels([User, Task, Events, Project]);
     await sequelize.sync();
     return sequelize;
   },
