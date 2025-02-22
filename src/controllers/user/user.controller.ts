7cffea028463b6d6ad7300ed09c300adcc70860f
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-02-22 22:59:40 +0700

    implement guard

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -7,12 +7,16 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 
+@ApiTags('User')
+@ApiBearerAuth()
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -27,18 +31,21 @@ export class UserController {
 
   @ApiOperation({ summary: 'Get all users' })
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.userService.findAll();
   }
 
   @ApiOperation({ summary: 'Get one user' })
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
     const userId = this.validateId(id);
     return this.userService.findOne(userId);
   }
 
   @ApiOperation({ summary: 'Create a new user' })
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
@@ -46,6 +53,7 @@ export class UserController {
 
   @ApiOperation({ summary: 'Update a user by id' })
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     const userId = this.validateId(id);
     return this.userService.update(userId, updateUserDto);
@@ -53,6 +61,7 @@ export class UserController {
 
   @ApiOperation({ summary: 'Delete a user by id' })
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     const userId = this.validateId(id);
     return this.userService.remove(userId);
