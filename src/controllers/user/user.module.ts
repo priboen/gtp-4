4e7126ebf25a7b71a19c5c91d2ef60013b5f672d
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-02-22 23:00:01 +0700

    implement JWT

--- src/controllers/user/user.module.ts
+++ src/controllers/user/user.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { userProvider } from 'src/common/providers';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService, userProvider],
 })
