5c0d007b048712cc655c00334d5be0db2031db3e
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-02-26 02:46:46 +0700

    register service provider

--- src/controllers/project/project.module.ts
+++ src/controllers/project/project.module.ts
@@ -1,17 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
-import { projectProvider } from 'src/common/providers';
+import { projectProvider, teamProjectProvider } from 'src/common/providers';
 import { JwtModule } from '@nestjs/jwt';
+import { TeamProjectService } from '../team-project/team-project.service';
+import { MysqlModule } from 'src/common/modules';
 
 @Module({
   imports: [
+    MysqlModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [ProjectController],
-  providers: [ProjectService, projectProvider],
+  providers: [
+    ProjectService,
+    projectProvider,
+    TeamProjectService,
+    teamProjectProvider,
+  ],
 })
 export class ProjectModule {}
