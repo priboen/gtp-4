28882237baab751b8185b7e1ea27d4a36d2b2377
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-02-26 01:24:05 +0700

    create event DTO

---
+++ src/controllers/events/dto/create-events.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, IsString, IsDateString, IsNumber } from 'class-validator';
+
+export class CreateEventsDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  date: Date;
+
+  @IsNotEmpty()
+  @IsNumber()
+  projectId: number;
+}
