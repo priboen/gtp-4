e559493471cba539484aa214f16c4ffb6f949691
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-02-26 12:11:52 +0700

    implement middleware

--- src/controllers/project/project.module.ts
+++ src/controllers/project/project.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
 import { projectProvider, teamProjectProvider } from 'src/common/providers';
 import { JwtModule } from '@nestjs/jwt';
 import { TeamProjectService } from '../team-project/team-project.service';
 import { MysqlModule } from 'src/common/modules';
+import { ProjectOwnerMiddleware } from 'src/common/middleware/project-owner.middleware';
 
 @Module({
   imports: [
@@ -22,4 +23,13 @@ import { MysqlModule } from 'src/common/modules';
     teamProjectProvider,
   ],
 })
-export class ProjectModule {}
+export class ProjectModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ProjectOwnerMiddleware)
+      .forRoutes(
+        { path: 'project/:id', method: RequestMethod.PATCH },
+        { path: 'project/:id', method: RequestMethod.DELETE },
+      );
+  }
+}
