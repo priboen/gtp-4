e224b229867eae13e001df38ebc81be4fb149b8f
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-03-03 05:03:06 +0700

    implement JWT and task provider

--- src/controllers/task/task.module.ts
+++ src/controllers/task/task.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { taskProvider } from 'src/common/providers';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [TaskController],
-  providers: [TaskService],
+  providers: [TaskService, taskProvider],
 })
 export class TaskModule {}
