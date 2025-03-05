a9bf826a4116d2d6d2fba4484100e86e86b8a398
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-03-06 01:24:04 +0700

    unimplement middleware

--- src/controllers/project/project.module.ts
+++ src/controllers/project/project.module.ts
@@ -5,7 +5,6 @@ import { projectProvider, teamProjectProvider } from 'src/common/providers';
 import { JwtModule } from '@nestjs/jwt';
 import { TeamProjectService } from '../team-project/team-project.service';
 import { MysqlModule } from 'src/common/modules';
-import { ProjectOwnerMiddleware } from 'src/common/middleware/project-owner.middleware';
 
 @Module({
   imports: [
@@ -22,14 +21,6 @@ import { ProjectOwnerMiddleware } from 'src/common/middleware/project-owner.midd
     TeamProjectService,
     teamProjectProvider,
   ],
+  exports: [ProjectService, projectProvider],
 })
-export class ProjectModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(ProjectOwnerMiddleware)
-      .forRoutes(
-        { path: 'project/:id', method: RequestMethod.PATCH },
-        { path: 'project/:id', method: RequestMethod.DELETE },
-      );
-  }
-}
+export class ProjectModule {}
