a6696e3785908413d594e6f12ce2c971a7869c60
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-03-06 03:38:19 +0700

    delete atributes

--- src/controllers/team-project/dto/create-team-project.dto.ts
+++ src/controllers/team-project/dto/create-team-project.dto.ts
@@ -3,9 +3,5 @@ import { IsNotEmpty, IsNumber } from 'class-validator';
 export class CreateTeamProjectDto {
   @IsNotEmpty()
   @IsNumber()
-  projectId: number;
-
-  @IsNotEmpty()
-  @IsNumber()
   userId: number;
 }
